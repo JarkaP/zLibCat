51739f9a1b3ade331cdc5888c90b4912c3896415
Jarka Pachlová <jarka.p@gmail.com>
2020-11-12 01:14:43 +0100

    add search

--- src/components/FilterTable.vue
+++ src/components/FilterTable.vue
@@ -1,5 +1,44 @@
 <template>
     <div>
+        <div
+            v-show="items.length > 0"
+            class="my-4 flex justify-between md:items-center flex-col md:flex-row"
+        >
+            <div class="flex-1 md:pr-4">
+                <div class="relative md:w-1/3 mb-4 md:mb-0">
+                    <input
+                        type="search"
+                        class="w-full pl-10 pr-4 py-2 rounded-lg shadow focus:outline-none focus:shadow-outline text-gray-600 font-medium"
+                        placeholder="Hledat"
+                        v-model="searchTermProxy"
+                    />
+                    <div
+                        class="absolute top-0 left-0 inline-flex items-center p-2"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="w-6 h-6 text-gray-400"
+                            viewBox="0 0 24 24"
+                            stroke-width="2"
+                            stroke="currentColor"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        >
+                            <rect
+                                x="0"
+                                y="0"
+                                width="24"
+                                height="24"
+                                stroke="none"
+                            ></rect>
+                            <circle cx="10" cy="10" r="7"></circle>
+                            <line x1="21" y1="21" x2="15" y2="15"></line>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div v-show="items.length > 0" class="overflow-x-auto">
             <div class="py-2 align-middle inline-block min-w-full">
                 <div
@@ -21,27 +60,27 @@
                             class="bg-white divide-y divide-gray-300 divide-dashed"
                         >
                             <tr
-                                v-for="item in items.slice(0, 5)"
+                                v-for="item in filteredItems"
                                 :key="item.id"
                                 class="even:bg-gray-100 hover:bg-purple-200"
                             >
                                 <td
-                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-800"
+                                    class="px-6 py-4 text-sm leading-5 text-gray-800"
                                 >
                                     {{ item.title }}
                                 </td>
                                 <td
-                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-800"
+                                    class="px-6 py-4 text-sm leading-5 text-gray-800"
                                 >
                                     {{ item.author }}
                                 </td>
                                 <td
-                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-800"
+                                    class="px-6 py-4 text-sm leading-5 text-gray-800"
                                 >
                                     {{ item.genre }}
                                 </td>
                                 <td
-                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-800"
+                                    class="px-6 py-4 text-sm leading-5 text-gray-800"
                                 >
                                     {{ item.id }}
                                 </td>
@@ -59,6 +98,7 @@
 
 <script>
 import axios from 'axios'
+import { debounce, deburr, includes, some } from 'lodash'
 import LoadingIndicator from './LoadingIndicator.vue'
 import AlertMessage from './AlertMessage.vue'
 
@@ -70,18 +110,32 @@ export default {
     },
     data() {
         return {
+            awaitingSearch: false,
             columns: ['Název', 'Autor', 'Žánr', 'Inv. číslo'],
             error: [],
             items: [],
             loading: false,
+            searchTerm: '',
+            searchTermProxy: '', // proxy for debouncing
         }
+    },
+    computed: {
+        filteredItems() {
+            const query = this.normalize(this.searchTerm)
+            return this.items.filter(item => {
+                // find a partial string in any property value of an object
+                return some(item, val => {
+                    return includes(this.normalize(val), query)
+                })
+            })
+        },
     },
     mounted: function() {
         this.getData()
     },
     methods: {
         getData: function() {
-            let self = this
+            const self = this
             self.loading = true
             axios
                 .get('/data/data.json')
@@ -102,6 +156,20 @@ export default {
                     self.loading = false
                 })
         },
+
+        normalize: function(str) {
+            return deburr(
+                str
+                    .toString()
+                    .trim()
+                    .toLowerCase()
+            )
+        },
+    },
+    watch: {
+        searchTermProxy: debounce(function(newVal) {
+            this.searchTerm = newVal
+        }, 500),
     },
 }
 </script>
